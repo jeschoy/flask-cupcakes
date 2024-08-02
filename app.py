e02d0c435703f444ab0489a52265fac473c3e0b3
Jessica Choi <jesymchoi@gmail.com>
2024-08-01 17:55:27 -0700

    completed backend api code

--- app.py
+++ app.py
@@ -1,1 +1,65 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, jsonify, request
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = "thisisasecret"
+
+app.app_context().push()
+connect_db(app)
+
+@app.route('/')
+def root():
+  """Show root page"""
+  return render_template('index.html')
+
+@app.route('/api/cupcakes')
+def show_cupcakes():
+  """Return all cupcakes in database"""
+  cupcakes = [cupcake.to_dict() for cupcake in Cupcake.query.all()]
+  return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake(cupcake_id):
+  """Show individual cupcake"""
+  cupcake = Cupcake.query.get_or_404(cupcake_id)
+  return jsonify(cupcake=cupcake.to_dict())
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+  data = request.json
+
+  cupcake = Cupcake(flavor=data['flavor'], size=data['size'], rating=data['rating'], image=data['image'] or None)
+  db.session.add(cupcake)
+  db.session.commit()
+  return (jsonify(cupcake=cupcake.to_dict()), 201)
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["PATCH"])
+def edit_cupcake(cupcake_id):
+  """To edit an exisiting cupcake"""
+  cupcake = Cupcake.query.get_or_404(cupcake_id)
+  data = request.json
+
+  """New data for cupcake"""
+  cupcake.flavor=data['flavor']
+  cupcake.size=data['size']
+  cupcake.rating=data['rating']
+  cupcake.image=data['image'] or None
+
+  db.session.add(cupcake)
+  db.session.commit()
+
+  return jsonify(cupcake=cupcake.to_dict())
+
+@app.route('/api/cupcakes/<int:cupcake_id>', methods=["DELETE"])
+def delete_cupcake(cupcake_id):
+  """To delete an existing cupcake from database"""
+  cupcake = Cupcake.query.get_or_404(cupcake_id)
+  db.session.delete(cupcake)
+  db.session.commit()
+  return jsonify(message="Deleted")
--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_URL = 'https://tinyurl.com/demo-cupcake'
+
+class Cupcake(db.Model):
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_URL)
+
+    def to_dict(self):
+        return {"id": self.id, "flavor": self.flavor, "size": self.size, "rating": self.rating, "image": self.image,}
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
